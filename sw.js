74971b4d2be043f835f2c1760a54e664ab15bf76
es1063122-creator <es1063122@gmail.com>
2025-11-28 14:15:51 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,20 @@
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open("safety-exam-cache").then(cache => {
+    caches.open("safety-exam-cache-v2").then(cache => {
       return cache.addAll([
         "./",
         "index.html",
-        "exams.js",
+        "exams-industrial.js",
+        "exams-construction.js",
         "manifest.json"
       ]);
     })
   );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(resp => {
-      return resp || fetch(event.request);
-    })
-  );
-});
+// 활성화 시 이전 캐시 제거 (캐시 꼬임 방지)
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.
